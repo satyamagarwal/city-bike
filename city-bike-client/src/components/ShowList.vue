7147c6b9812096d6153fa351e274276ade078bb6
satyam-agarwal <satyam.agarwal@finn.no>
2022-10-03 01:40:34 +0200

    Introduce pagination

--- city-bike-client/src/components/ShowList.vue
+++ city-bike-client/src/components/ShowList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="osg-grid osg-grid--gap-row osg-margin-top-4 osg-margin-bottom-4">
+  <div class="osg-grid osg-grid--gap-row osg-margin-bottom-2">
     <div class="osg-grid__column--4 osg-color-bg-blue-dark">
       <div class="osg-full-height osg-flex osg-flex-justify-content-center osg-flex-align-items-center osg-color-text-white osg-text-size-india">
         Stasjons navn
@@ -21,31 +21,38 @@
       </div>
     </div>
   </div>
-  <template v-if="filteredStations.length > 0">
-    <template v-for="stationAndStatus in filteredStations" :key="stationAndStatus.id">
+  <template v-if="stationsWithPageNumber.length > 0">
+    <template v-for="station in stationsWithPageNumber[currentPageNumber - 1]" :key="station.id">
       <div class="osg-grid osg-grid--gap-row">
         <div class="osg-grid__column--4 osg-color-bg-blue-dark osg-padding-2">
           <div class="osg-full-height osg-flex osg-flex-justify-content-center osg-flex-align-items-center osg-color-text-white">
-            {{ stationAndStatus.info.name }}
+            {{ station.info.name }}
           </div>
         </div>
         <div class="osg-grid__column--4 osg-color-bg-blue-light">
           <div class="osg-full-height osg-flex osg-flex-justify-content-center osg-flex-align-items-center osg-color-text-black">
-            {{ stationAndStatus.info.address }}
+            {{ station.info.address }}
           </div>
         </div>
         <div class="osg-grid__column--2 osg-color-bg-blue-dark">
           <div class="osg-full-height osg-flex osg-flex-justify-content-center osg-flex-align-items-center osg-color-text-white">
-            {{ stationAndStatus.status.numBikesAvailable }}
+            {{ station.status.numBikesAvailable }}
           </div>
         </div>
         <div class="osg-grid__column--2 osg-color-bg-blue-light">
           <div class="osg-full-height osg-flex osg-flex-justify-content-center osg-flex-align-items-center osg-color-text-black">
-            {{ stationAndStatus.status.numDocksAvailable }}
+            {{ station.status.numDocksAvailable }}
           </div>
         </div>
       </div>
     </template>
+    <div class="osg-grid__column--4 osg-padding-2">
+      <div id="vue-pagination" class="osg-full-height osg-flex osg-flex-justify-content-center osg-flex-align-items-center">
+        <Pagination aria-label="Pagination, first page" :disable-current-index-btn="false" :current-index="currentPageNumber"
+                    :total-pages="totalPages" v-on:paginate="(num) => currentPageNumber = num" :limit="10">
+        </Pagination>
+      </div>
+    </div>
   </template>
   <template v-else>
     <div class="osg-grid osg-grid--gap-row">
@@ -59,21 +66,67 @@
 </template>
 
 <script>
+import Pagination from "./pagination.vue"
+
 export default {
   name: "ShowList",
   props: {
     searchParam: String,
     stationsAndStatuses: Array
   },
-  data () {
+  components: {
+    Pagination
+  },
+  data() {
     return {
-      search: ''
+      search: '',
+      pageNumber: 1
     }
   },
   computed: {
+    stationsWithPageNumber() {
+      const list = this.filteredStations()
+
+      return list
+          .reduce(
+              (result, currentValue) => {
+                if (currentValue) {
+                  const pageNo = Math.floor(list.indexOf(currentValue) / 20)
+
+                  if (result[pageNo]) {
+                    result[pageNo].includes(currentValue) ? [] : result[pageNo].push(currentValue)
+                  } else {
+                    result[pageNo] = [currentValue]
+                  }
+                }
+
+                return result
+              },
+              []
+          )
+    },
+    totalPages() {
+      const pages = this.stationsWithPageNumber.length
+
+      return pages > 0 ? pages : 1
+    },
+    currentPageNumber: {
+      get() {
+        return this.pageNumber
+      },
+      set(newPageNumber) {
+        this.totalPages >= newPageNumber ? this.pageNumber = newPageNumber : this.pageNumber = 1
+      }
+    }
+  },
+  watch: {
+    searchParam() {
+      this.currentPageNumber = 1
+    }
+  },
+  methods: {
     filteredStations() {
       const lowerCasedSearch = this.searchParam.toLocaleLowerCase()
-
       return this
           .stationsAndStatuses
           .filter((stationAndStatus) =>
---
+++ city-bike-client/src/components/pagination.vue
@@ -0,0 +1,88 @@
+<template>
+  <nav class="osg-pagination" :aria-label="ariaLabel">
+    <button class="osg-pagination__previous" v-show="showArrows && currentIndex > 1" @click.prevent="paginate(currentIndex - 1)">
+      <span class="osg-sr-only">{{ i18n.previousBtn }} {{ currentIndex - 1 }}</span>
+    </button>
+    <template v-for="index in totalPages" :key="index">
+      <button v-if="showItem(index)" class="osg-pagination__item" v-bind:class="{ 'osg-pagination__item--current': index === currentIndex, 'osg-pagination__item--rectangle': index >= 100 }" :key="index" :disabled="index === currentIndex && disableCurrentIndexBtn" @click.prevent="paginate(index)">
+        <span class="osg-sr-only">{{ i18n.showPage }} {{ index }}</span>
+        {{ index }}
+      </button>
+      <span v-else class="osg-pagination__spacer" aria-hidden="true">&hellip;</span>
+    </template>
+    <button class="osg-pagination__next" v-show="showArrows && currentIndex < totalPages" @click.prevent="paginate(currentIndex + 1)">
+      <span class="osg-sr-only">{{ i18n.nextBtn }} {{ currentIndex + 1 }}</span>
+    </button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "OsgPagination",
+  emits: ['paginate'],
+  props: {
+    ariaLabel: {
+      type: String,
+      required: true,
+    },
+    totalPages: {
+      type: Number,
+      required: true,
+    },
+    currentIndex: {
+      type: Number,
+      required: true,
+    },
+    disableCurrentIndexBtn: {
+      type: Boolean,
+      default: true,
+    },
+    showArrows: {
+      type: Boolean,
+      default: true,
+    },
+    threshold: {
+      type: Number,
+      default: 10,
+    },
+    limit: {
+      type: Number,
+      default: 2,
+    },
+    i18n: {
+      type: Object,
+      default: () => {
+        return {
+          previousBtn: "Show previous page",
+          nextBtn: "Show next page",
+          showPage: "Show page",
+        };
+      },
+    },
+  },
+  computed: {
+    limitMax: function () {
+      return this.currentIndex + this.limit;
+    },
+    limitMin: function () {
+      return this.currentIndex - this.limit;
+    },
+  },
+  methods: {
+    showItem: function (index) {
+      const isFirst = index === 1;
+      const isLast = index === this.totalPages;
+      const isWithinLimit = index >= this.limitMin && index <= this.limitMax;
+      const threshold = this.totalPages <= this.threshold;
+
+      return isFirst || isLast || isWithinLimit || threshold;
+    },
+    showSpacer: function (index) {
+      return this.totalPages > this.threshold && index >= this.limitMin - 1 && index <= this.limitMax + 1;
+    },
+    paginate: function (value) {
+      this.$emit("paginate", value);
+    }
+  },
+};
+</script>
