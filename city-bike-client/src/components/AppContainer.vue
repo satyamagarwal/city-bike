2e663110c60d02a1460aa3e02616c7271ec89cf4
satyamagarwal <agarwal.satyam3@gmail.com>
2022-11-01 11:51:36 +0530

    Restructure vue component (#3)

--- city-bike-client/src/components/AppContainer.vue
+++ city-bike-client/src/components/AppContainer.vue
@@ -2,36 +2,25 @@
   <header class="osg-header">
     <div class="osg-header__logo">
       <figure class="osg-logo">
-        <img src="./../assets/oslo_logo.svg" alt="Oslo Kommune logo"/>
+        <img
+          src="./../assets/oslo_logo.svg"
+          alt="Oslo Kommune logo"
+        >
       </figure>
     </div>
-    <div class="osg-header__content">
-      <div class="osg-none osg-block-breakpoint-large osg-full-height">
-        <div class="osg-grid osg-grid--gap osg-full-height">
-          <div class="osg-grid__column--5">
-            <div
-                class="osg-full-height osg-flex osg-flex-justify-content-center osg-flex-align-items-center osg-text-size-golf"
-            >
-              Oslo Bysykkle: Stasjon søk
-            </div>
-          </div>
-          <div class="osg-grid__column--7">
-            <div class="osg-full-height osg-flex osg-flex-justify-content-flex-end osg-flex-align-items-center">
-              <input v-model="search" class="osg-input__input" type="text" autocomplete="on" aria-label="Label text"
-                     placeholder="Søk etter et address" data-testid="search-address"/>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <PageHeader @search="searchParam" />
   </header>
   <div class="osg-container">
-    <ShowList :stationsAndStatuses="stationsAndStatuses" :searchParam="search"/>
+    <ShowList
+      :stations-and-statuses="searchedStationsAndStatuses"
+      :search-param="searchTerm"
+    />
   </div>
 </template>
 
 <script>
 import ShowList from "./ShowList.vue";
+import PageHeader from "./PageHeader.vue";
 import {config} from 'icefog'
 
 const host = config.isProd ? '' : 'http://localhost:8080'
@@ -39,31 +28,49 @@ const host = config.isProd ? '' : 'http://localhost:8080'
 export default {
   name: "AppContainer",
   components: {
+    PageHeader,
     ShowList
   },
   data() {
     return {
-      search: '',
-      stationsAndStatuses: []
-    }
-  },
-  methods: {
-    async fetchInfoAndStatus() {
-      const response = await fetch(`${host}/station-status`)
-
-      if (response.ok) {
-        const content = await response.json()
-        this.stationsAndStatuses = content
-            .sort((station1, station2) => station1.info.name.localeCompare(station2.info.name))
-      } else {
-        this.stationsAndStatuses = []
-      }
+      defaultStationsAndStatuses: [],
+      searchedStationsAndStatuses: [],
+      searchTerm: '',
     }
   },
   mounted() {
-    this.fetchInfoAndStatus()
+    this.getInfoAndStatus()
 
-    setInterval(this.fetchInfoAndStatus, 10000);
+    setInterval(this.getInfoAndStatus, 10000);
+  },
+  methods: {
+    async getInfoAndStatus() {
+      const response = await fetch(`${host}/station-status`)
+      const content = response.ok ? await response.json() : []
+      const sortedList = content.sort((station1, station2) => station1.info.name.localeCompare(station2.info.name))
+
+      this.defaultStationsAndStatuses = sortedList
+
+      if (this.searchedStationsAndStatuses.length === 0) {
+        this.searchedStationsAndStatuses = sortedList
+      }
+    },
+    searchParam(param) {
+      if (param) {
+        const lowerCasedSearch = this.searchTerm.toLocaleLowerCase()
+
+        this.searchTerm = param
+        this.searchedStationsAndStatuses = this
+            .defaultStationsAndStatuses
+            .filter((stationAndStatus) =>
+                stationAndStatus.info.name.toLocaleLowerCase().includes(lowerCasedSearch) ||
+                stationAndStatus.info.address.includes(lowerCasedSearch)
+            )
+      } else {
+        this.searchTerm = ''
+        this.searchedStationsAndStatuses = this.defaultStationsAndStatuses
+      }
+    }
   }
 }
 </script>
---
+++ city-bike-client/src/components/DataUnavailable.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="osg-grid osg-grid--gap-row">
+    <div class="osg-grid__column--12 osg-padding-2">
+      <div
+        class="osg-full-height osg-flex osg-flex-justify-content-center osg-flex-align-items-center osg-color-text-red"
+      >
+        Data er ikke tilgjengelig!
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DataUnavailable"
+}
+</script>
